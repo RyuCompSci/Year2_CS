023092d6753d1af2d5a8b826d073d5c4ca758abc
RyuCompSci <89348765+RyuCompSci@users.noreply.github.com>
2022-11-07 20:26:36 +0900

    Create quiz_070.py

---
+++ Quizzes/quiz_070.py
@@ -0,0 +1,27 @@
+### Boolean
+
+```.py
+class Boolean_go:
+    def Boo_go(self, a, b, c):
+        ab = 0
+        nb = 0
+        ncb = 0
+        if a == b == 1:
+            ab = 1
+        else:
+            ab = 0
+        if b == 0:
+            nb = 1
+        else:
+            nb = 0
+        if c == b == 1:
+            ncb = 0
+        else:
+            ncb = 1
+        if ab + nb + ncb == 0:
+            print(0)
+        else:
+            print(0)
+```
+
+![]()
